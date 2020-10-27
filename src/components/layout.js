cd9bebe9804288bed3c2b4ad95d2d942178a6603
iosonosempreio <iosonosempreio@users.noreply.github.com>
2020-10-27 02:56:59 +0100

    Update layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,7 +5,7 @@ import StickyMessage from "./stickyMessage"
 export default function Layout(props) {
   return (
     <>
-      <Navigation navBackground={PaymentResponse.navBackground} />
+      <Navigation navBackground={props.navBackground} />
       <StickyMessage />
       <div className="layout-main">{props.children}</div>
       <Footer />
