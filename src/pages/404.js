907e58c1e2cb3faa29ccbe45650485e084e9f172
iosonosempreio <iosonosempreio@users.noreply.github.com>
2020-11-01 17:14:14 +0100

    Update 404.js

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,15 @@
 import React from "react"
 import { Link } from "gatsby"
 import Layout from "../components/layout"
+import styles from "../styles/page.module.css"
 
 export default function NotFound() {
   return (
     <Layout title="404 - Pagina non trovata">
-      <p className={["paragraph", "paragraphBig"].join(" ")}>
+      <p className={["paragraph", "paragraphBig", styles.paragraph].join(" ")}>
         Questa pagina non esiste o non è ancora stata creata.
       </p>
-      <p className="paragraph">
+      <p className={["paragraph", styles.paragraph].join(" ")}>
         Controlla che l'indirizzo sia corretto, oppure ritorna alla{" "}
         <Link to="/">home</Link>.
       </p>
