c0d0278de3b555dbecf54838d0662d3a897d5e18
iosonosempreio <iosonosempreio@users.noreply.github.com>
2025-11-08 08:03:23 +0100

    Update diventa-socio.js

--- pages/diventa-socio.js
+++ pages/diventa-socio.js
@@ -14,26 +14,23 @@ export default function DiventaSocio() {
 				<Row className="my-5">
 					<Col md={{ span: 7, offset: 1 }}>
 						<h5>
-							È possibile iscriversi ufficialmente al libro soci
-							dell’associazione compilando il modulo di tesseramento e versando
-							la quota associativa annuale.
+							È possibile iscriversi ufficialmente al libro soci dell’associazione facendo richiesta al consiglio di
+							amministrazione e versando la quota associativa annuale.
 						</h5>
 						<img src={tesseramento.src} className="img-fluid rounded-3 mb-3" />
 						<p>
-							Scarica e stampa il modulo, compilalo in ogni sua parte e
-							consegnalo alla prima occasione. Nel frattempo inviaci una
-							scansione per mail (info [@] abilitiamo.org) così da predisporre
-							la tua iscrizione.
+							Invia una mail a risorseumane@abilitiamo.org per chiedere il modulo di iscrizione, compilalo in ogni sua
+							parte e consegnalo alla prima occasione. Nel frattempo invia una scansione alla stessa mail così da
+							predisporre la tua iscrizione.
 						</p>
 						<p>
-							<strong>In seguito all'approvazione del consiglio direttivo</strong>, potrai
-							versare la quota associativa di €30 (trenta/00) sul c/c {info.associazione.iban}.
+							<strong>In seguito all'approvazione del consiglio direttivo</strong>, potrai versare la quota associativa
+							di €30 (trenta/00) sul c/c {info.associazione.iban}.
 						</p>
 						<p>
-							L’informativa sulla privacy è disponibile a{" "}
-							<Link href="/privacy">questa pagina</Link>.
+							L’informativa sulla privacy è disponibile a <Link href="/privacy">questa pagina</Link>.
 						</p>
-						<ArrowLink data={{label:"Scarica modulo tesseramento", url:moduloPdf, download:true}} />
+						{/* <ArrowLink data={{label:"Scarica modulo tesseramento", url:moduloPdf, download:true}} /> */}
 					</Col>
 					<Col>
 						<div className={classNames("position-sticky")} style={{ top: 65 }}>
